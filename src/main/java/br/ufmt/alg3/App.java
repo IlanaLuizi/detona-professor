1a9922a6753898b6e54f6487bd8da490607eeaca
Ilana Luizi <ilanaluizy1008@gmail.com>
2024-07-31 21:33:37 -0400

    demonstra

--- src/main/java/br/ufmt/alg3/App.java
+++ src/main/java/br/ufmt/alg3/App.java
@@ -1,5 +1,7 @@
 package br.ufmt.alg3;
 
+import java.util.Scanner;
+
 import br.ufmt.alg3.curso.Curso;
 import br.ufmt.alg3.professor.Professor;
 
@@ -11,20 +13,44 @@ public class App
 {
     public static void main( String[] args )
     {
-        Curso c = new Curso(); /*1 Curso é a classe, 2 c é a variável, "new Curso()" está criando o objeto curso// nesse momento está salvando em c a referência  */ /*variável só guarda uma referência */
-        c.setNome("SI"); /* está indo na referência e colocando "SI" */
-        System.out.println(c.getNome());
+    /*  Curso[] respostas;
+        respostas = new Curso[3]; instanciando array 
 
-        Professor p = new Professor(); /*reservando o endereço na memória, até esse momento Professor tem null */
-        p.setNome ("Rafael");
-        p.setArea_atuacao("TI");
-        p.setDisciplina("Algoritmos III");
-        p.setSemestre("2024/1");
+        int [][] mat = new int [3][2]; instanciando martriz  */
 
-        System.out.println(p.getNome());
+        Curso[] respostas;
+        respostas = new Curso[3];
+        Scanner teclado;
+        teclado = new Scanner(System.in);
+        for(int i = 0; i < respostas.length; i++){
+            Curso c = new Curso();
+            System.out.print("Digite o nome do Curso:");
+            String nome = teclado.nextLine();
+            c.setNome(nome);
+            System.out.println("o nome digitado foi:" + c.getNome());
+            respostas[i] = c;
+        }
+        for (Curso curso : respostas){
+            System.out.println("Curso avaliado:" + curso.getNome());
 
-        /*garbage collector - na execução */
-        /*new Curso() é chamado de método construtor */
+        }
+        
 
+        
+  
     }
 }
+
+ /*garbage collector - na execução */
+        /*new Curso() é chamado de método construtor */
+        
+        /*cd detona-professor
+         * git init
+         * sudo apt install git
+         * git add pom.xml src/*
+         * git status
+         * git config user.name "Ilana Luizi"
+         * git config user.email ilanaluizy1008@gmail.com
+         * git commit -m "inicia o projeto"
+         */
+
